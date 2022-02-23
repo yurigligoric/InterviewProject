8b02ab8598a3a44281ef09894ee09866dcd991da
libra34567 <libra.li@hotmail.com>
2022-02-23 04:05:58 -0700

    spawning and despawning

--- Assets/CharacterSpawner.cs
+++ Assets/CharacterSpawner.cs
@@ -4,10 +4,29 @@ public class CharacterSpawner : MonoBehaviour
 {
     [SerializeField] private Transform spawnPosition;
     [SerializeField] private string spherePoolItemName;
-
+    
+    private GameObject _spawnedObj;
+    public GameObject SpawnedSphereObj => _spawnedObj;
+    
     // Start is called before the first frame update
     void Start()
+    { 
+        _spawnedObj = ServiceManager.Instance.poolService.Spawn(spherePoolItemName, spawnPosition.position, Quaternion.identity);
+    }
+
+    private void Update()
     {
-        ServiceManager.Instance.poolService.Spawn(spherePoolItemName, spawnPosition.position, Quaternion.identity);
+        //Despawn
+        if (Input.GetKeyDown(KeyCode.Q) && _spawnedObj != null)
+        {
+            ServiceManager.Instance.poolService.Despawn(_spawnedObj);
+            _spawnedObj = null;
+        }
+        
+        //spawn
+        if (Input.GetKeyDown(KeyCode.E) && _spawnedObj == null)
+        {
+            _spawnedObj = ServiceManager.Instance.poolService.Spawn(spherePoolItemName, spawnPosition.position, Quaternion.identity);
+        }
     }
 }
